c9e64919eb25da6ad509410dff433eeb2c2be100
MKhuphuli <nmkhuphuli@gmail.com>
2018-07-10 14:56:48 +0200

    Deleted commented out code (that used ggmap for plotting)

--- malApp.R
+++ malApp.R
@@ -98,46 +98,9 @@ server <- function(input, output, session) {
     my_map <- leaflet(df) %>% 
       addTiles() %>% 
       addCircles(~longitude, ~latitude, weight = 3, radius=40)
-      #addCircleMarkers(lng=~longitude,lat=~latitude, weight=1, radius=2, fillOpacity = 0.8 )
     my_map})
   
- ' 
-  output$map = renderPlot({
-    
-    mal_data <- get_data_fromDB(credentials=credentials, get_malaria_Data)
-    df1 <- reported_case_counts(df=mal_data)
-  
-    if(input$province=="all_p"){
-      df1 <- df1[substr(df1$date_reported, 1, 10) <= input$date,] # select date
-      df<- na.omit(df1[, c("longitude", "latitude")]) 
-      zoom <- 5
-    } else {
-      df1 <- df1[df1$province==input$province,]
-      df1 <- df1[substr(df1$date_reported, 1, 10) <= input$date,] # select date
-      df <- na.omit(df1[ c("longitude", "latitude")])
-      zoom <- 6
-    }
-
-    gps_of_provinces <- read.csv("gps_of_provinces.csv", header=TRUE)
-    row.names(gps_of_provinces)<-c("lon","lat")
-    
-
-    if(nrow(df1[df1$province==input$province,])==0 & input$province!="all_p"){
-      #no data in province
-      mymap <- get_map(location= c(lon=gps_of_provinces["lon","all_p" ],lat=gps_of_provinces["lat", "all_p" ]),  
-                     maptype = "roadmap",zoom=zoom, scale=2)
-      ggmap(mymap) 
-      
-      } else {
-        mymap <- get_map(location= c(lon=gps_of_provinces["lon",input$province ],lat=gps_of_provinces["lat",input$province ]),
-                     maptype = "roadmap",zoom=zoom, scale=2)
-        ggmap(mymap) +
-          geom_point(data= df, aes(x=longitude, y=latitude, fill="red", alpha = 0.8), size=1, shape=21) +
-          guides(fill=FALSE, alpha=FALSE, size=FALSE)
-        
-        }
-  }, width=1000,height=1000)'
-  
+ 
   output$timeSeriesGraph <- renderPlot({
     
     time_series <- get_data_fromDB(credentials=credentials, get_time_Data)
